f544b11078ae957bff650b1da6264467db387f7a
LuanLopesbr2102 <luanlopesbr2102@gmail.com>
2024-03-09 22:57:49 -0300

    feat: Correção na segurança

--- Auth/funcoesJWT.js
+++ Auth/funcoesJWT.js
@@ -1,5 +1,5 @@
 //Essa funções tem como por objetivo gerar token de acesso / tem como objetivo verificar se o token é válido ou não 
-/*import jwt from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 export function assinar(usuario){
     const token = jwt.sign({usuario}, process.env.SECRET, {expiresIn:'300s'});
     return token;
@@ -7,4 +7,4 @@ export function assinar(usuario){
 
 export function verificarAssinatura(token){
     return jwt.verify(token, process.env.SECRET);
-}*/
+}
