c89e0f9fa70ee1434a31ec091b576a3973d39fda
LuanLopesbr2102 <luanlopesbr2102@gmail.com>
2024-03-12 01:42:14 -0300

    feat: Correção de commit

--- routes/rotaLogin.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express"; 
-import { autenticar } from "../Auth/Autenticacao.js";
-
-const rotaLogin = new Router();
-rotaLogin.post('/',(req, res) =>{
-    autenticar(req, res);
-})
-
-export default rotaLogin
